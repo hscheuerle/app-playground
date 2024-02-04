660c0d81a60f4de5f9bda035ccb2c5eedc0f2b60
hscheue <hscheue@gmail.com>
2024-02-04 14:30:25 -0500

    update memory to class

--- express/session/memory.mjs
+++ express/session/memory.mjs
@@ -1,136 +1,65 @@
 import Store from "./store.mjs";
-import { inherits } from "util";
 
-/**
- * A session store in memory.
- * @public
- */
-
-export default class MemoryStore {
+export default class MemoryStore extends Store {
   constructor() {
-    this.call(this); // review this
+    super();
     this.sessions = Object.create(null);
   }
-}
 
-/**
- * Inherit from Store.
- */
+  all(callback) {
+    var sessionIds = Object.keys(this.sessions);
+    var sessions = Object.create(null);
 
-inherits(MemoryStore, Store);
+    for (var i = 0; i < sessionIds.length; i++) {
+      var sessionId = sessionIds[i];
+      var session = getSession.call(this, sessionId);
 
-/**
- * Get all active sessions.
- *
- * @param {function} callback
- * @public
- */
-
-MemoryStore.prototype.all = function all(callback) {
-  var sessionIds = Object.keys(this.sessions);
-  var sessions = Object.create(null);
-
-  for (var i = 0; i < sessionIds.length; i++) {
-    var sessionId = sessionIds[i];
-    var session = getSession.call(this, sessionId);
-
-    if (session) {
-      sessions[sessionId] = session;
+      if (session) {
+        sessions[sessionId] = session;
+      }
     }
+
+    callback && defer(callback, null, sessions);
   }
 
-  callback && defer(callback, null, sessions);
-};
-
-/**
- * Clear all sessions.
- *
- * @param {function} callback
- * @public
- */
-
-MemoryStore.prototype.clear = function clear(callback) {
-  this.sessions = Object.create(null);
-  callback && defer(callback);
-};
-
-/**
- * Destroy the session associated with the given session ID.
- *
- * @param {string} sessionId
- * @public
- */
-
-MemoryStore.prototype.destroy = function destroy(sessionId, callback) {
-  delete this.sessions[sessionId];
-  callback && defer(callback);
-};
-
-/**
- * Fetch session by the given session ID.
- *
- * @param {string} sessionId
- * @param {function} callback
- * @public
- */
-
-MemoryStore.prototype.get = function get(sessionId, callback) {
-  defer(callback, null, getSession.call(this, sessionId));
-};
-
-/**
- * Commit the given session associated with the given sessionId to the store.
- *
- * @param {string} sessionId
- * @param {object} session
- * @param {function} callback
- * @public
- */
-
-MemoryStore.prototype.set = function set(sessionId, session, callback) {
-  this.sessions[sessionId] = JSON.stringify(session);
-  callback && defer(callback);
-};
-
-/**
- * Get number of active sessions.
- *
- * @param {function} callback
- * @public
- */
-
-MemoryStore.prototype.length = function length(callback) {
-  this.all(function (err, sessions) {
-    if (err) return callback(err);
-    callback(null, Object.keys(sessions).length);
-  });
-};
-
-/**
- * Touch the given session object associated with the given session ID.
- *
- * @param {string} sessionId
- * @param {object} session
- * @param {function} callback
- * @public
- */
-
-MemoryStore.prototype.touch = function touch(sessionId, session, callback) {
-  var currentSession = getSession.call(this, sessionId);
-
-  if (currentSession) {
-    // update expiration
-    currentSession.cookie = session.cookie;
-    this.sessions[sessionId] = JSON.stringify(currentSession);
+  clear(callback) {
+    this.sessions = Object.create(null);
+    callback && defer(callback);
   }
 
-  callback && defer(callback);
-};
+  destroy(sessionId, callback) {
+    delete this.sessions[sessionId];
+    callback && defer(callback);
+  }
 
-/**
- * Get session from the store.
- * @private
- */
+  get(sessionId, callback) {
+    defer(callback, null, getSession.call(this, sessionId));
+  }
+
+  set(sessionId, session, callback) {
+    this.sessions[sessionId] = JSON.stringify(session);
+    callback && defer(callback);
+  }
+
+  length(callback) {
+    this.all(function (err, sessions) {
+      if (err) return callback(err);
+      callback(null, Object.keys(sessions).length);
+    });
+  }
+
+  touch(sessionId, session, callback) {
+    var currentSession = getSession.call(this, sessionId);
+
+    if (currentSession) {
+      // update expiration
+      currentSession.cookie = session.cookie;
+      this.sessions[sessionId] = JSON.stringify(currentSession);
+    }
+
+    callback && defer(callback);
+  }
+}
 
 function getSession(sessionId) {
   var sess = this.sessions[sessionId];
